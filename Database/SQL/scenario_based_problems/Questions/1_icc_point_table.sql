7febcf0a683860a72d96d00316c610f5fd102707
Maniarasan <69270126+bell003@users.noreply.github.com>
2024-06-26 19:30:50 +0530

    Create 1_icc_point_table.sql

---
+++ Database/SQL/scenario_based_problems/Questions/1_icc_point_table.sql
@@ -0,0 +1,17 @@
+/*
+You have the ICC tournament winners' details. Please find the team,  No of matches played, winning count, and losses.
+*/
+
+--Create table & insert data
+create table icc_world_cup(
+Team_1 Varchar(20),
+Team_2 Varchar(20),
+Winner Varchar(20)
+);
+
+INSERT INTO icc_world_cup values('India','SL','India');
+INSERT INTO icc_world_cup values('SL','Aus','Aus');
+INSERT INTO icc_world_cup values('SA','Eng','Eng');
+INSERT INTO icc_world_cup values('Eng','NZ','NZ');
+INSERT INTO icc_world_cup values('Aus','India','India');
+
