1e1f1eaa31fc3370a079b7781a36e9c8bfd51c54
Maniarasan <69270126+bell003@users.noreply.github.com>
2024-06-30 07:37:45 +0530

    Create 4_find_day_occurrence.sql

---
+++ Database/SQL/scenario_based_problems/Questions/4_find_day_occurrence.sql
@@ -0,0 +1,7 @@
+/*
+Find the nth occurrence of the particulare day from given date
+example: 
+  given date is 2024-06-30 , 
+  find the upcoming first/second/third sunday.
+  which is 2024-07-07/2024-07-14/2024-07-21
+*/
